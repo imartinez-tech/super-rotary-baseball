a1e5f8e0f1e4b8b35d4aec0761e94c49f8061a6c
ibraim martinez <ibraimm5@gmail.com>
2021-11-04 18:32:04 -0400

    adding thee starter files and images

---
+++ client/src/components/logos/mlb.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+import { TOR } from 'react-mlb-logos';
+const Example = () => {
+    return ( 
+    <div> 
+        <TOR />
+    </div>
+    );
+};
+
+export default Example; 
